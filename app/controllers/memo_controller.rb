27af978f625f213fdee8d56bf5851aa7f931361c
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-04-13 22:56:33 +0900

    Add create

--- app/controllers/memo_controller.rb
+++ app/controllers/memo_controller.rb
@@ -1,11 +1,23 @@
 class MemoController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
-    # @post = Post.new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(set_post)
+    if @post.save
+      redirect_to root_path
+    else
+      render 'memo/new'
+    end
   end
+
+  private
+    def set_post
+      params.require(:post).permit(:text, :created_at)
+    end
 end
